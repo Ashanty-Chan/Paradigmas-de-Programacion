7f565f9df7fb91de1d40534973ad452432443022
Ashanty-Chan <ashantychan508@gmail.com>
2024-10-17 21:21:21 -0500

    13_UtilizarFunciones.py

---
+++ 13_UtilizarFunciones.py
@@ -0,0 +1,13 @@
+#================================
+# Chan Campos Ashanty Iyari
+# ESFM IPN Matemática Algorítmica
+# Octubre del 2024
+#================================
+
+
+#====================================================
+# De funciones.py traer las funciones suma y promedio
+#====================================================
+from funciones import suma,promedio
+print(suma(300,500))
+print(promedio(100,700))
---
+++ funciones.py
@@ -0,0 +1,18 @@
+#================================
+# Chan Campos Ashanty Iyari
+# ESFM IPN Matemática Algorítmica
+# Octubre del 2024
+#================================
+
+#====================================
+# Funciones suma, promedio y potencia
+#====================================
+def suma(x,y):
+    return x+y
+
+def promedio(x,y):
+    return 0.25*(x+y)
+
+def potencia(x,y):
+    return x**y
+
