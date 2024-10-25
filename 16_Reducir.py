d80f13ce49beba4eb54cf4c5dfb7a94f6ed11769
Ashanty-Chan <ashantychan508@gmail.com>
2024-10-24 18:52:18 -0600

    16_Reducir.py

--- 16_Reducir.py
+++ 16_Reducir.py
@@ -3,3 +3,27 @@
 # IPN ESFM Licenciatura en Matemática Algorítmica
 # Octubre 2024
 #================================================
+
+#=========================================
+# Uso de reducciones (colapsar resultados)
+#=========================================
+
+#================================================
+# Multiplicación de todos los números en la lista
+#================================================
+from functools import reduce
+bigdata = [7,10,9,22,12,6,4,19,1,45]
+
+#=============
+# Función x*y
+#=============
+multiplicar = lambda x,y: x*y
+
+print(reduce(multiplicar,bigdata))
+print(reduce(suma,bigdata))
+
+#=========================================================
+# Reduce le aplica la función por pares a los resultados y
+# el siguiente elemento comenzando con los dos primeros
+# elementos
+#==========================================================
