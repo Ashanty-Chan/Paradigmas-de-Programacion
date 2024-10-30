f5034739ed55b160e0e177ab92cb6ba9d7c3abf5
Ashanty-Chan <ashantychan508@gmail.com>
2024-10-29 19:07:36 -0600

    01_PythonBasico.py

--- 01_PythonBasico.py
+++ 01_PythonBasico.py
@@ -32,14 +32,14 @@ print(type(t))  #real (flotante)
 t=True
 print(type(t))  #booleano (bool)
 
-#=======================================
+#=====================
 # Mensajes a pantalla
-#======================================
+#=====================
 print("ESte es un comando de python. ","ESte es otro enunciado.", t)
 print('id: ', 1)
 print('Nombres:', 'Steve')
 print('Apellidos: ', 'Jobs')
-print(("vamos a sumar esto"+" con esto otro")
+print("vamos a sumar esto" + " con esto otro")
 
 #======================================
 #Continuar una instrucción en varios renglones
@@ -58,8 +58,8 @@ print("Hola "); print("tu")
 # Usando paréntesis redondos, cuadrados o llaves
 # se puede escribir en varios renglones
 #===========================================
-lista = [1, 2, 3, 4, 
-        5, 6, 7, 8, 
+lista = [1, 2, 3, 4,
+        5, 6, 7, 8,
         9, 10, 11, 12]
 
 matriz = [ [1, 2, 3, 4],[5,6,7,8],[9,10,11,12] ]
@@ -80,10 +80,10 @@ if 10>2:
    print("verdadero")
    if 5<30:
       print("verdadero")
-elif 7>2   # comienza segundo condicional, así mismo "elif" es para evaluar una cadena de múltiples condiciones
-print("esto no se imprimira")
-else: 
-print("aquí nunca llega")
+elif 7>2:   # comienza segundo condicional, así mismo "elif" es para evaluar una cadena de múltiples condiciones
+    print("esto no se imprimirá")
+else:
+    print("aquí nunca llega")
 
 #==============
 # Funciones
@@ -93,68 +93,3 @@ def saludar(nombre):
    print("Bienvenido al tutorial de Python")
 
 saludar("Ashanty")
-
-   
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
