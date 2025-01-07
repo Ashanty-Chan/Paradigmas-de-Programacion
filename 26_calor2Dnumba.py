9f89c9096c04bf8f46c602898104642e5903cb9f
Ashanty-Chan <ashantychan508@gmail.com>
2025-01-07 01:55:40 -0600

    26_calor2Dnumba.py

--- 26_calor2Dnumba.py
+++ 26_calor2Dnumba.py
@@ -55,7 +55,7 @@ un = np.zeros(nt,dtype=np.float64)  # arreglo de escritura
 def evolucion(u,n,udx2,dt,i,k):
     jp1 = i + n[0]
     jm1 = i - n[0]
-    laplaciano = (u[i-1]-2.0*u[i]+u[i+1])*udx2[0] + (u[jm1]-2.0*u[i]+u[jp1])*udx2[1]
+    laplaciano = (u[i - 1] - 2.0 * u[i] + u[i + 1]) * udx2[0] + (u[jm1] - 2.0 * u[i] + u[jp1]) * udx2[1]
     unueva = u[i] + dt*k*laplaciano
     return unueva
 
@@ -67,7 +67,8 @@ def solucion(u,un,udx2,dt,n,k):
     for jj in range(1,n[1]-1):
         for ii in range(1,n[0]-1):
             i = ii + n[0]*jj
-            unueva = evolucion(u,un,udx2,dt,n,k)
+            # Pass the correct index 'i' to evolucion:
+            unueva = evolucion(u,n,udx2,dt,i,k)
             if i == int(nt/2)+int(n[0]/2):
                 unueva = 1.0
             un[i] = unueva
@@ -91,9 +92,6 @@ x,y = np.meshgrid(np.arange(0,L[0],dx[0]),np.arange(0,L[1],dx[1]))
 ax = plt.axes(projection='3d')
 ax.plot_surface(x,y,u,cmap=cm.hsv)
 plt.show()
-
-
-
 
 
 
